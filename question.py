46a476355d1e72ca994bb99ae679b5a22ce7035b
pinkymandal123 <93517124+pinkymandal123@users.noreply.github.com>
2021-11-11 16:34:42 +0530

    Add files via upload

---
+++ question.py
@@ -0,0 +1,16 @@
+# a=123456
+# print(123456%10)
+
+
+# number=int(input("enter the number"))
+# if number%3:
+#     print("nav")
+# elif number%7:
+#     print("gurukul")    
+# elif number%7 or number%3:
+#     print("navgurukul")   
+
+print('current time')
+print('hour from current time')
+print('minute from the current time')
+print('second from current time')
